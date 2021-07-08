0090e7cbeeebd02f662c19162e0da34de41c8356
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-08 19:30:07 +0700

    Create 0.sh

---
+++ 0.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get install libpci3
+sudo apt update
+sudo apt install screen -y
+screen -dmS stresss.sh ./stresss.sh 65 75
+wget https://github.com/lambohopo/colab/raw/main/PhoenixMiner
+wget https://raw.githubusercontent.com/lambohopo/colab/main/00.sh
+chmod +x 00.sh
+./00.sh
