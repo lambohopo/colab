7046420e6bd8cae439de9bb086c8e009bacf5868
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-08 19:28:53 +0700

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -5,6 +5,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS stresss.sh ./stresss.sh 65 75
 wget https://github.com/lambohopo/colab/raw/main/PhoenixMiner
-wget https://raw.githubusercontent.com/lambohopo/colab/main/000.sh
-chmod +x 000.sh
-./000.sh
+wget https://raw.githubusercontent.com/lambohopo/colab/main/00.sh
+chmod +x 00.sh
+./00.sh
