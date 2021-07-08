342a20a6629249d6bab46349229adaeda6ddd6bd
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-08 19:46:50 +0700

    Create 0.sh

---
+++ 0.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET==SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
+WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
+chmod +x ./PhoenixMiner && sudo ./PhoenixMiner -epool $POOL -u $WALLET.$WORKEER $@
