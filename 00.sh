590896abdbd5d33e73d38d6013eb98d815b9f91f
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-08 19:19:57 +0700

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -2,4 +2,4 @@
 POOL=ethash.unmineable.com:3333
 WALLET==SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
 WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
-chmod +x ./PhoenixMiner && sudo ./PhoenixMiner -a ethash -o  $POOL -u $WALLET.$WORKER $@
+chmod +x ./PhoenixMiner && sudo ./PhoenixMiner -a ethash -epool $WALLET.$WORKER $@ -pass x
