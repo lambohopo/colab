7f13cff5facca7226026d36186fcacc0e2a16700
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-10-19 09:31:55 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -3,4 +3,4 @@ wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.
 
 tar xf hellminer_cpu_linux.tar.gz
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJfxuiyjCUm9w7Xryn5ssNF6voJKRBdRGR.BR11 -p x --cpu 40
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.pp -p x --cpu 8
