c91d692dd3a33b8e76932b0a41fe43ac3988c168
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-10-28 21:12:47 +0700

    Create gcep.sh

---
+++ gcep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt install screen -y
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
+while [ 2 ]; do
+screen ./nheqminer -v -l ap.luckpool.net:3956 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.gcep -p x -t 6
+sleep 2
+done
+exit
