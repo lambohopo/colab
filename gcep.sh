467e3640e5c4ba8744f3d476de1c0213143b8a80
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-12-09 14:00:26 +0700

    Update gcep.sh

--- gcep.sh
+++ gcep.sh
@@ -5,7 +5,7 @@ tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
 while [ 2 ]; do
-screen ./nheqminer -v -l ue.luckpool.net:3956 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.gcep -p --hybrid x -t 4
+screen ./nheqminer -v -l eu.luckpool.net:3956 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.gcep -p --hybrid x -t 4
 sleep 2
 done
 exit
